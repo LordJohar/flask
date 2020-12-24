34393e7813068ef06b9d638f408c748209ebada9
Lord Johar <johar133@gmail.com>
2020-12-24 18:36:51 +0000

    user model add

--- authz/model/user.py
+++ authz/model/user.py
@@ -1,4 +1,9 @@
 from authz import db
+from shared import uuidgen
+
 
 class User(db.Model):
-    
+
+    id=db.Column(db.String(64), primary_key=True, default=uuidgen)
+    username=db.Column(db.String(128), unique=True, index=True, nullable=False)
+    password=db.Column(db.String(256), nullable=False)
