5076424a8134cb585d4091b89b14500d70f044ee
Lord Johar <johar133@gmail.com>
2020-12-27 20:10:00 +0000

    Change Schema user.py

--- authz/controller/apiv1/user.py
+++ authz/controller/apiv1/user.py
@@ -6,13 +6,15 @@ from authz.schema.apiv1 import UserSchema
 class UserController:
 
     def get_users():
-        # users = User.query.all()
-        # users_schema = UserSchema(many=True)
-        print("GET ALL USERS")
-        # return users_schema.dump(users)
+         users = User.query.all()
+         users_schema = UserSchema(many=True)
+       
+         return users_schema.dump(users)
 
     def get_user(user_id):
+        
         user = User.query.get(user_id)
+
         user_schema = UserSchema()
         if user is None:
             abort(404)
--- authz/schema/apiv1/user.py
+++ authz/schema/apiv1/user.py
@@ -4,8 +4,8 @@ from authz.model import User
 
 class UserSchema(ma.SQLAlchemySchema):
     class Meta:
-        model: User
+        model = User
 
     id = ma.auto_field(dump_only=True)
     username = ma.auto_field()
-    password = ma.auto_field()
+    password = ma.auto_field(load_only=True)
